bf1cfc2c0808fb50d00706df23d51c35d1eed009
rahmadani <dedekemoking@gmail.com>
2026-07-16 01:44:47 +0700

    bump v1.1.0

--- app/src/main/java/com/billingps/aptv/models/MainViewModel.kt
+++ app/src/main/java/com/billingps/aptv/models/MainViewModel.kt
@@ -76,7 +76,7 @@ fun defaultPaketDurasi(): Map<String, Int> = mapOf(
     "2 Jam" to 120, "3 Jam" to 180, "Main Bebas" to 0,
 )
 
-    const val APP_VERSION = "1.0.9"
+    const val APP_VERSION = "1.1.0"
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
